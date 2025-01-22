628c5b0709582be4c299b598068ef81cb14bb8bd
Francis Chartrand <chartrandf@users.noreply.github.com>
2025-01-22 16:33:46 -0500

    fix: migrate good values for bandwith

--- dev/bweforvideo.js
+++ dev/bweforvideo.js
@@ -1,15 +1,13 @@
 getStatsParser.bweforvideo = function(result) {
     if (result.type === 'candidate-pair') {
         getStatsResult.bandwidth.availableSendBandwidth = result.availableOutgoingBitrate || 0;
-        getStatsResult.bandwidth.googAvailableSendBandwidth = result.availableOutgoingBitrate || 0;
-        getStatsResult.bandwidth.googAvailableReceiveBandwidth = result.availableIncomingBitrate || 0; // @todo not available anymore, check when using ICE
-        getStatsResult.bandwidth.googTransmitBitrate = result.bytesSent || 0;
     };
 
     if (result.type === 'outbound-rtp') {
-        getStatsResult.bandwidth.googBucketDelay = result.packetsSent > 0 ? result.totalPacketSendDelay / result.packetsSent : 0;
-        getStatsResult.bandwidth.googTargetEncBitrate = result.headerBytesSent + result.bytesSent;
-        getStatsResult.bandwidth.googActualEncBitrate = result.bytesSent - result.retransmittedBytesSent;
-        getStatsResult.bandwidth.googRetransmitBitrate = result.retransmittedBytesSent;
+        getStatsResult.bandwidth.transmitBitrate = (result.headerBytesSent + result.bytesSent) || 0;
+        getStatsResult.bandwidth.bucketDelay = result.packetsSent > 0 ? result.totalPacketSendDelay / result.packetsSent : 0;
+        getStatsResult.bandwidth.targetEncBitrate = result.targetBitrate;
+        getStatsResult.bandwidth.actualEncBitrate = result.bytesSent - result.retransmittedBytesSent;
+        getStatsResult.bandwidth.retransmitBitrate = result.retransmittedBytesSent;
     }
 };
