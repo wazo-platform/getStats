9fa040ac9144d38aec78715e99a3402fd6ed19c7
Francis Chartrand <chartrandf@users.noreply.github.com>
2025-01-22 15:39:08 -0500

    feat(video): track stats for quality

--- dev/googCodecName.video.js
+++ dev/googCodecName.video.js
@@ -4,19 +4,13 @@ getStatsParser.checkVideoTracks = function(result) {
     var sendrecvType = getStatsResult.internal.getSendrecvType(result);
     if (!sendrecvType) return;
 
-    const rtpResultKey = sendrecvType === 'send' ? 'outbound-rtp' : 'inbound-rtp';
-    // @todo double check this logic when having multiple candidates
-    // @todo refator... rtpResultKey, can be result.type
-    const rtpResult = getRtpResult(getStatsResult.results, rtpResultKey, 'video');
+    const rtpResult = getRtpResult(getStatsResult.results, result.type, 'video');
     if (!rtpResult) return;
 
-    if (!rtpResult) return;
+    const remoteRtpResult = getStatsResult.results.find(r => r.id === result.remoteId) || {};
 
     const codecResult = getCodecResult(getStatsResult.results, rtpResult.codecId);
     if (!codecResult) return;
-
-    const mediaPlayoutResult = getStatsResult.results.find(r => r.type === 'media-playout');
-    if (!mediaPlayoutResult) return;
 
     const currentCodec = getCodecName(codecResult.mimeType) || 'VP8';
     if (getStatsResult.video[sendrecvType].codecs.indexOf(currentCodec) === -1) {
@@ -24,7 +18,6 @@ getStatsParser.checkVideoTracks = function(result) {
     }
 
     if (!!result.bytesSent) {
-        var kilobytes = 0;
         if (!getStatsResult.internal.video[sendrecvType].prevBytesSent) {
             getStatsResult.internal.video[sendrecvType].prevBytesSent = result.bytesSent;
         }
@@ -32,14 +25,12 @@ getStatsParser.checkVideoTracks = function(result) {
         var bytes = result.bytesSent - getStatsResult.internal.video[sendrecvType].prevBytesSent;
         getStatsResult.internal.video[sendrecvType].prevBytesSent = result.bytesSent;
 
-        kilobytes = bytes / 1024;
-
-        getStatsResult.video[sendrecvType].availableBandwidth = kilobytes.toFixed(1);
-        getStatsResult.video.bytesSent = kilobytes.toFixed(1);
+        var kilobytes = bytes / 1024;
+        getStatsResult.video[sendrecvType].bytesSent = kilobytes;
+        getStatsResult.video.bytesSent = kilobytes;
     }
 
     if (!!result.bytesReceived) {
-        var kilobytes = 0;
         if (!getStatsResult.internal.video[sendrecvType].prevBytesReceived) {
             getStatsResult.internal.video[sendrecvType].prevBytesReceived = result.bytesReceived;
         }
@@ -47,9 +38,9 @@ getStatsParser.checkVideoTracks = function(result) {
         var bytes = result.bytesReceived - getStatsResult.internal.video[sendrecvType].prevBytesReceived;
         getStatsResult.internal.video[sendrecvType].prevBytesReceived = result.bytesReceived;
 
-        kilobytes = bytes / 1024;
-        // getStatsResult.video[sendrecvType].availableBandwidth = kilobytes.toFixed(1);
-        getStatsResult.video.bytesReceived = kilobytes.toFixed(1);
+        var kilobytes = bytes / 1024;
+        getStatsResult.video[sendrecvType].bytesReceived = kilobytes;
+        getStatsResult.video.bytesReceived = kilobytes;
     }
 
     if (rtpResult.frameHeight && result.frameWidth) {
@@ -61,72 +52,35 @@ getStatsParser.checkVideoTracks = function(result) {
         getStatsResult.video[sendrecvType].tracks.push(result.trackIdentifier);
     }
 
-    // @todo do not to existing in the migration guide and note inside the payload of result
-    if (result.framerateMean) {
-        getStatsResult.bandwidth.framerateMean = result.framerateMean;
-        var kilobytes = 0;
-        if (!getStatsResult.internal.video[sendrecvType].prevFramerateMean) {
-            getStatsResult.internal.video[sendrecvType].prevFramerateMean = result.bitrateMean;
-        }
-
-        var bytes = result.bytesSent - getStatsResult.internal.video[sendrecvType].prevFramerateMean;
-        getStatsResult.internal.video[sendrecvType].prevFramerateMean = result.framerateMean;
-
-        kilobytes = bytes / 1024;
-        getStatsResult.video[sendrecvType].framerateMean = bytes.toFixed(1);
+    // Frames Per Second (FPS) refers to the rate at which video frames are being sent or received
+    if (result.framesPerSecond) {
+        getStatsResult.video[sendrecvType].framesPerSecond = result.framesPerSecond;
     }
 
-    // @todo do not to existing in the migration guide and note inside the payload of result
-    if (result.bitrateMean) {
-        getStatsResult.bandwidth.bitrateMean = result.bitrateMean;
-        var kilobytes = 0;
-        if (!getStatsResult.internal.video[sendrecvType].prevBitrateMean) {
-            getStatsResult.internal.video[sendrecvType].prevBitrateMean = result.bitrateMean;
+    // Frames Sent refers to the total number of video frames that have been sent since the start of the session
+    if (Number.isInteger(result.framesSent)) {
+        if (!getStatsResult.internal.video[sendrecvType].prevFramesSent) {
+            getStatsResult.internal.video[sendrecvType].prevFramesSent = result.framesSent;
         }
 
-        var bytes = result.bytesSent - getStatsResult.internal.video[sendrecvType].prevBitrateMean;
-        getStatsResult.internal.video[sendrecvType].prevBitrateMean = result.bitrateMean;
+        var newFramesSent = result.framesSent - getStatsResult.internal.video[sendrecvType].prevFramesSent;
+        getStatsResult.internal.video[sendrecvType].prevFramesSent = result.framesSent;
 
-        kilobytes = bytes / 1024;
-        getStatsResult.video[sendrecvType].bitrateMean = bytes.toFixed(1);
+        getStatsResult.video[sendrecvType].framesSent = newFramesSent;
     }
 
-    // calculate latency
-    // @todo latency still not working as expected... do not expect 0 or below ...
-    // Possible interesting values
-    // 'remote-inbout-rpt' (kind, roundTripTime, roundTripTimeMeasurements?)
-    console.log('video', rtpResult, rtpResult.jitterBufferDelay);
-    const currentDelayMs = (rtpResult.jitterBufferDelay || 0) + mediaPlayoutResult.totalPlayoutDelay;
-    if (!!currentDelayMs) {
-        var kilobytes = 0;
-        if (!getStatsResult.internal.video.prevGoogCurrentDelayMs) {
-            getStatsResult.internal.video.prevGoogCurrentDelayMs = currentDelayMs;
-        }
+    // Video quality
+    getStatsResult.video[sendrecvType].totalRoundTripTime = remoteRtpResult.totalRoundTripTime || 0;
+    getStatsResult.video[sendrecvType].jitter = remoteRtpResult.jitter || 0;
+    getStatsResult.video[sendrecvType].jitterBufferDelay = remoteRtpResult.jitterBufferDelay || 0;
+    getStatsResult.video[sendrecvType].packetsLost = result.type === 'inbound-rtp' ? rtpResult.packetsLost : remoteRtpResult.packetsLost;
+    getStatsResult.video[sendrecvType].packetsReceived = result.type === 'inbound-rtp' ? rtpResult.packetsReceived : 0;
 
-        var bytes = currentDelayMs - getStatsResult.internal.video.prevGoogCurrentDelayMs;
-        getStatsResult.internal.video.prevGoogCurrentDelayMs = currentDelayMs;
-
-        getStatsResult.video.latency = bytes.toFixed(1);
-
-        if (getStatsResult.video.latency < 0) {
-            getStatsResult.video.latency = 0;
-        }
+    if(remoteRtpResult.totalRoundTripTime) {
+        getStatsResult.video.latency = remoteRtpResult.totalRoundTripTime;
     }
 
-    // calculate packetsLost
-    if (!!result.packetsLost) {
-        var kilobytes = 0;
-        if (!getStatsResult.internal.video.prevPacketsLost) {
-            getStatsResult.internal.video.prevPacketsLost = result.packetsLost;
-        }
-
-        var bytes = result.packetsLost - getStatsResult.internal.video.prevPacketsLost;
-        getStatsResult.internal.video.prevPacketsLost = result.packetsLost;
-
-        getStatsResult.video.packetsLost = bytes.toFixed(1);
-
-        if (getStatsResult.video.packetsLost < 0) {
-            getStatsResult.video.packetsLost = 0;
-        }
+    if (Number.isInteger(rtpResult.packetsLost)) {
+        getStatsResult.video.packetsLost = rtpResult.packetsLost;
     }
 };
